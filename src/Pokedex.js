ba7f5fea14a06924f75c8206f87461fc1e8f7106
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-10-13 09:12:16 -0400

    Add conditional logic to indicate which player wins and which player loses.

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -8,12 +8,12 @@ import './Pokedex.css';
 
 class Pokedex extends Component {
   render() {
-    const { cards, p1Total, p2Total } = this.props;
+    const { cards, total, isWinner } = this.props;
 
     return (
       <div className='Pokedex'>
-        {p1Total > p2Total ? <h2>Winning Hand</h2> : <h2>Losing Hand</h2>}
-        <p className='Pokedex-total'>Total Experience: {p1Total || p2Total}</p>
+        <h2 className={isWinner ? 'Pokedex-winner' : 'Pokedex-loser'}>{isWinner ? 'Winner' : 'Loser'}</h2>
+        <p className='Pokedex-total'>Total Experience: {total}</p>
         <div className='Pokedex-cards'>
           {cards.map((card, key) => (
             <Pokecard key={key} card={card} />
--- src/Pokegame.js
+++ src/Pokegame.js
@@ -5,65 +5,58 @@ import Pokedex from './Pokedex';
 
 // COMPONENT STYLES
 import './Pokegame.css';
-import { tsConstructorType } from '../node_modules/@babel/types';
 
 class Pokegame extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      cards: [
-        {
-          id: 4,
-          name: 'Charmander',
-          type: 'fire',
-          base_experience: 62
-        },
-        {
-          id: 7,
-          name: 'Squirtle',
-          type: 'water',
-          base_experience: 63
-        },
-        {
-          id: 11,
-          name: 'Metapod',
-          type: 'bug',
-          base_experience: 72
-        },
-        {
-          id: 12,
-          name: 'Butterfree',
-          type: 'flying',
-          base_experience: 178
-        },
-        {
-          id: 25,
-          name: 'Pikachu',
-          type: 'electric',
-          base_experience: 112
-        },
-        {
-          id: 39,
-          name: 'Jigglypuff',
-          type: 'normal',
-          base_experience: 95
-        },
-        {
-          id: 94,
-          name: 'Gengar',
-          type: 'poison',
-          base_experience: 225
-        },
-        {
-          id: 133,
-          name: 'Eevee',
-          type: 'normal',
-          base_experience: 65
-        }
-      ]
+  cards = [
+    {
+      id: 4,
+      name: 'Charmander',
+      type: 'fire',
+      base_experience: 62
+    },
+    {
+      id: 7,
+      name: 'Squirtle',
+      type: 'water',
+      base_experience: 63
+    },
+    {
+      id: 11,
+      name: 'Metapod',
+      type: 'bug',
+      base_experience: 72
+    },
+    {
+      id: 12,
+      name: 'Butterfree',
+      type: 'flying',
+      base_experience: 178
+    },
+    {
+      id: 25,
+      name: 'Pikachu',
+      type: 'electric',
+      base_experience: 112
+    },
+    {
+      id: 39,
+      name: 'Jigglypuff',
+      type: 'normal',
+      base_experience: 95
+    },
+    {
+      id: 94,
+      name: 'Gengar',
+      type: 'poison',
+      base_experience: 225
+    },
+    {
+      id: 133,
+      name: 'Eevee',
+      type: 'normal',
+      base_experience: 65
     }
-  }
+  ];
 
   p1Total = 0;
   p2Total = 0;
@@ -90,12 +83,12 @@ class Pokegame extends Component {
   }
 
   render() {
-    this.imgSrc(this.state.cards);
+    this.imgSrc(this.cards);
 
-    this.shuffle(this.state.cards);
+    this.shuffle(this.cards);
 
-    const p1Cards = this.state.cards.slice(0, 4);
-    const p2Cards = this.state.cards.slice(4);
+    let p1Cards = this.cards.slice(0, 4);
+    let p2Cards = this.cards.slice(4);
 
     p1Cards.forEach(card => {
       this.p1Total += card.base_experience;
@@ -108,8 +101,8 @@ class Pokegame extends Component {
     return (
       <div className='Pokegame'>
         <h1 id='Pokegame-title'>Pokegame</h1>
-        <Pokedex cards={p1Cards} p1Total={this.p1Total} />
-        <Pokedex cards={p2Cards} p2Total={this.p2Total} />
+        <Pokedex cards={p1Cards} total={this.p1Total} isWinner={this.p1Total > this.p2Total} />
+        <Pokedex cards={p2Cards} total={this.p2Total} isWinner={this.p2Total > this.p1Total} />
       </div>
     );
   }
